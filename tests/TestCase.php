2f3b646ecc357da91aa824f98c89b22e9d902e84
Filip Fonal <filip.fonal@escolasoft.com>
2022-08-26 23:58:57 +0200

    Update

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,34 +3,14 @@
 namespace FilipFonal\FilamentLogManager\Tests;
 
 use FilipFonal\FilamentLogManager\FilamentLogManagerServiceProvider;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'FilipFonal\\FilamentLogManager\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
-    protected function getPackageProviders($app)
+    protected function getPackageProviders($app): array
     {
         return [
             FilamentLogManagerServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_filament-log-manager_table.php.stub';
-        $migration->up();
-        */
     }
 }
