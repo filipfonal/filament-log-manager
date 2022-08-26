3feda2d8c0a3e34b32f80cfb5d7838a20f34e264
Filip Fonal <filip.fonal@escolasoft.com>
2022-08-26 22:34:15 +0200

    Create view

--- resources/views/pages/logs.blade.php
+++ resources/views/pages/logs.blade.php
@@ -1,60 +1,47 @@
 <x-filament::page>
     <div class="flex items-center justify-between">
-        <div class="w-full">
+        <div class="w-full mr-2">
             {{ $this->search }}
+        </div>
+        <div class="w-auto ml-2">
+            <x-filament::button :disabled="is_null($this->logFile)" type="button" color="danger">
+                Usuń
+            </x-filament::button>
+        </div>
+        <div class="w-auto ml-2">
+            <x-filament::button :disabled="is_null($this->logFile)" type="button" color="primary">
+                Pobierz
+            </x-filament::button>
         </div>
     </div>
     <x-filament::hr />
     <div>
-        <div id="accordion" role="tablist" aria-multiselectable="true">
+        <div>
             <div x-data="{ opened_tab: null }" class="flex flex-col">
-            @forelse($logs as $key => $log)
+            @forelse($this->getLogs() as $key => $log)
                     <div
-                            @class([
-                                'bg-white border border-gray-300 shadow-sm rounded-xl relative',
-                                'dark:bg-gray-800 dark:border-gray-600' => config('forms.dark_mode'),
-                            ])
+                            class="rounded-xl relative mb-2 py-3 px-3 bg-{{ $log['level_class'] }}"
+                            :class="{'no-bottom-radius mb-0': opened_tab == {{$key}}}"
                     >
-                        <a @click="opened_tab = opened_tab == {{$key}} ? null : {{$key}} "
-                                @class([
-                                       'flex items-center h-10 overflow-hidden border-b bg-gray-50 rounded-t-xl',
-                                       'dark:bg-gray-800 dark:border-gray-700' => config('forms.dark_mode'),
-                                   ])
+                        <a
+                                @click="opened_tab = opened_tab == {{$key}} ? null : {{$key}} "
+                                style="cursor: pointer"
+                                class="block overflow-hidden rounded-t-xl text-white"
                         >
-                                <i class="la la-{{ $log['level_img'] }}"></i>
                                 <span>[{{ $log['date'] }}]</span>
-                                {{ Str::limit($log['text'], 150) }}
+                                {{ Str::limit($log['text'], 100) }}
                         </a>
-                        <div x-show="opened_tab=={{$key}}" class="px-4 pb-4">
-                            <div class="panel-body">
-                                <p>{{$log['text']}}</p>
-                                <pre><code class="php">{{ trim($log['stack']) }}</code></pre>
-                            </div>
+                    </div>
+                    <div x-show="opened_tab=={{$key}}" class="mb-2 px-4 py-4 bg-white dark:bg-gray-700 text-gray-900 dark:text-white rounded-xl no-top-radius">
+                        <div>
+                            <p>{{$log['text']}}</p>
+                            @if(!empty($log['stack']))
+                                <div class="bg-gray-100 dark:bg-gray-900 mt-4 p-4 text-sm opacity-40">
+                                    <pre class="overflow-scroll"><code>{{ trim($log['stack']) }}</code></pre>
+                                </div>
+                            @endif
                         </div>
                     </div>
-{{--                <div--}}
-{{--                        @class([--}}
-{{--                            'accordion-item bg-white border border-gray-300 shadow-sm rounded-xl relative',--}}
-{{--                            'dark:bg-gray-800 dark:border-gray-600' => config('forms.dark_mode'),--}}
-{{--                        ])--}}
-{{--                >--}}
-{{--                        <h2 class="accordion-header mb-0 bg-{{ $log['level_class'] }}" role="tab" id="heading{{ $key }}">--}}
-{{--                            <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse{{ $key }}" aria-expanded="true" aria-controls="collapse{{ $key }}" class="text-white">--}}
-{{--                                <i class="la la-{{ $log['level_img'] }}"></i>--}}
-{{--                                <span>[{{ $log['date'] }}]</span>--}}
-{{--                                {{ Str::limit($log['text'], 150) }}--}}
-{{--                            </a>--}}
-{{--                        </h2>--}}
-{{--                        <div id="collapse{{ $key }}" class="accordion-collapse collapse show" aria-labelledby="heading{{ $key }}"--}}
-{{--                             data-bs-parent="#accordionExample">--}}
-{{--                            <div class="accordion-body py-4 px-5">--}}
-{{--                                <div class="panel-body">--}}
-{{--                                    <p>{{$log['text']}}</p>--}}
-{{--                                    <pre><code class="php">{{ trim($log['stack']) }}</code></pre>--}}
-{{--                                </div>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                </div>--}}
             @empty
                 <h3 class="text-center">No Logs to display.</h3>
             @endforelse
