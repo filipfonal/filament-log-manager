e4cf253ee0c76fcb15d0f403e13512867fde0fa3
Carlo Eusebi <carloeusebi@gmail.com>
2024-12-26 18:18:52 +0100

    Added italian translation

---
+++ resources/lang/it/translations.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'navigation_label' => 'Logs',
+    'navigation_group' => 'System',
+    'title' => 'Logs',
+    'search_placeholder' => 'Seleziona o cerca un file di log',
+    'no_logs' => 'Nessun file di log',
+    'delete' => 'Elimina',
+    'download' => 'Download',
+    'no_such_file' => 'Nessun file di log con questo nome',
+    'file_too_large' => 'File troppo grande per essere visualizzato',
+    'modal_delete_heading' => 'Elimina file di log?',
+    'modal_delete_subheading' => 'Sei sicuro di voler eliminare questo file di log?',
+    'modal_delete_action_cancel' => 'Annulla',
+    'modal_delete_action_confirm' => 'Elimina',
+];
